3382f7d4c1d1916ba4a683042e787a48a3714c73
Billy Sidharta <100707365+B-Sidharta@users.noreply.github.com>
2024-02-07 16:08:55 -0700

    updated Id.cs model

--- ViewpointAPI/Models/Id.cs
+++ ViewpointAPI/Models/Id.cs
@@ -1,7 +1,17 @@
-﻿namespace ViewpointAPI.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ViewpointAPI.Models
 {
+    [BsonIgnoreExtraElements]
     public class Id : SecurityData
     {
         //define variables for this collection here
+
+        [BsonElement("identifier")]
+        public string Identifier { get; set; } = string.Empty; // User input: Human readable security name. 
+
+        [BsonElement("id_bb_global")]
+        public string Id_bb_global { get; set; } = string.Empty; // Bloomberg global security identifier. 
     }
 }
